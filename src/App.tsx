9fd2831bd671be041aa2c3427f4c2a2040600167
vrut <brugnara@spaziodati.eu>
2021-07-02 21:24:51 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Layout} from './layout/Layout'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export class App extends React.PureComponent {
+  public render(): React.ReactNode {
+    return (
+      <div className="App">
+        <Layout />
+      </div>
+    );
+}
 }
 
 export default App;
